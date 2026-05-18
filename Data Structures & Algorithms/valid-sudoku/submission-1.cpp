faabe4f915a8215db5829d5ebd2cb5d62e184b1e
sanoje123 <119084835+sanoje123@users.noreply.github.com>
2026-05-18 15:38:48 +0200

    Add: valid-sudoku - submission-1

---
+++ Data Structures & Algorithms/valid-sudoku/submission-1.cpp
@@ -0,0 +1,30 @@
+#import<unordered_map>
+#import<unordered_set>
+#import<utility>
+
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+
+        unordered_map<int, unordered_set<char>> rows, cols;
+        map<pair<int, int>, unordered_set<char>> squares;
+
+        for (int r = 0; r < 9; ++r) {
+            for (int c = 0; c < 9; ++c) {
+                if (board[r][c] == '.') continue;
+
+                pair<int, int> square_key = {r / 3, c / 3};
+
+                if (rows[r].count(board[r][c]) || cols[c].count(board[r][c]) || squares[square_key].count(board[r][c])) {
+                    return false;
+                }
+
+                rows[r].insert(board[r][c]);
+                cols[c].insert(board[r][c]);
+                squares[square_key].insert(board[r][c]);
+            }
+        }
+
+        return true;
+    }
+};
