aeeb28cbd1a06f3efc7fa3b9f76ddee4a0d15060
sanoje123 <119084835+sanoje123@users.noreply.github.com>
2026-06-23 14:07:12 +0200

    Add: balanced-binary-tree - submission-2

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-2.py
@@ -0,0 +1,53 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        
+        # # Recursion:
+        # def dfs(root):
+        #     if not root:
+        #         return [True, 0]
+            
+        #     left, right = dfs(root.left), dfs(root.right)
+        #     balanced = (left[0] and right[0] and abs(left[1] - right[1]) <= 1)
+
+        #     return [balanced, 1 + max(left[1], right[1])]
+
+        # return dfs(root)[0]
+            
+        # Withouth recursion dfs:
+
+        stack = []
+        node = root
+        last = None
+        depths = {}
+
+        while stack or node:
+            if node:
+                stack.append(node)
+                node = node.left
+            else:
+                node = stack[-1]
+                if not node.right or last == node.right:
+                    stack.pop()
+                    left = depths.get(node.left, 0)
+                    right = depths.get(node.right, 0)
+
+                    if abs(left - right) > 1:
+                        return False
+
+                    depths[node] = 1 + max(left, right)
+                    last = node
+                    node = None
+
+                else:
+                    node = node.right
+            
+        return True
+
+        
